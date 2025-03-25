d1181c95047b5e611623d34f2bd90ecd0a6cc084
Sergey <serezham13@gmail.com>
2025-03-25 14:08:00 +0700

    Реализует паттерн «Адаптер»

--- src/api/events-api-service.js
+++ src/api/events-api-service.js
@@ -14,33 +14,34 @@ export default class EventsApiService extends ApiService {
 
   get points() {
     return this._load({ url: 'points' })
-      .then(ApiService.parseResponse);
+      .then(ApiService.parseResponse)
+      .then((points) => points.map(this.#adaptToClient));
   }
 
   async addPoint(point) {
     const response = await this._load({
       url: 'points',
       method: Method.POST,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({ 'Content-Type': 'application/json' }),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
-    return parsedResponse;
+    return this.#adaptToClient(parsedResponse);
   }
 
   async updatePoint(point) {
     const response = await this._load({
       url: `points/${point.id}`,
       method: Method.PUT,
-      body: JSON.stringify(point),
+      body: JSON.stringify(this.#adaptToServer(point)),
       headers: new Headers({ 'Content-Type': 'application/json' }),
     });
 
     const parsedResponse = await ApiService.parseResponse(response);
 
-    return parsedResponse;
+    return this.#adaptToClient(parsedResponse);
   }
 
   async deletePoint(pointId) {
@@ -52,5 +53,50 @@ export default class EventsApiService extends ApiService {
     const parsedResponse = await ApiService.parseResponse(response);
 
     return parsedResponse;
+  }
+
+  #adaptToClient(point) {
+    const adaptedPoint = {
+      ...point,
+      id: point.id,
+      basePrice: point.base_price,
+      dateFrom: new Date(point.date_from),
+      dateTo: new Date(point.date_to),
+      destination: point.destination,
+      favorite: point.is_favorite,
+      offersId: point.offers,
+      type: point.type,
+    };
+
+    // Удаляем ненужные ключи
+    delete adaptedPoint.base_price;
+    delete adaptedPoint.date_from;
+    delete adaptedPoint.date_to;
+    delete adaptedPoint.is_favorite;
+    delete adaptedPoint.offers;
+
+    return adaptedPoint;
+  }
+
+  #adaptToServer(point) {
+    const adaptedPoint = {
+      ...point,
+      base_price: point.basePrice,
+      date_from: point.dateFrom.toISOString(),
+      date_to: point.dateTo.toISOString(),
+      destination: point.destination,
+      is_favorite: point.favorite,
+      offers: point.offersId,
+      type: point.type,
+    };
+
+    // Удаляем ненужные ключи
+    delete adaptedPoint.basePrice;
+    delete adaptedPoint.dateFrom;
+    delete adaptedPoint.dateTo;
+    delete adaptedPoint.favorite;
+    delete adaptedPoint.offersId;
+
+    return adaptedPoint;
   }
 }
